2e05fa6164b0f84bd89f6142b79387ee75a79de6
Patrick Maley <pmaley@email.arizona.edu>
2016-02-23 13:56:05 -0700

    Initial commit with framework

--- src/controller/JukeBox.java
+++ src/controller/JukeBox.java
@@ -1,5 +1,28 @@
 package controller;
 
-public class JukeBox {
+import java.util.HashMap;
 
+import model.Song;
+
+public class JukeBox {
+	
+	HashMap<Integer, Song> songCollection;
+	
+	public JukeBox(){
+		songCollection = new HashMap<>();
+	}
+	
+	public void setupView(){
+		
+	}
+	
+	public boolean canPlay(Song song){
+		return false;
+		
+	}
+	
+	public void addToPlayList(Song song){
+		
+	}
 }
+
